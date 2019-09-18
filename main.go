68e5fb3891cb236a11d0678ad5817af8df4ce8e6
Sher Chowdhury <sher.chowdhury@ibm.com>
2019-09-18 14:29:42 +0100

    wip

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"text/template"
 )
 
 func main() {
-	fmt.Println("hello world")
+	type EnvironmentConfigs struct {
+		EnvName string
+	}
+
+	devEnv := EnvironmentConfigs{
+		"Dev",
+	}
+	// Here we created the tmplContent object which contains a sample template to be renderred.
+	tmplContent, _ := template.New("test").Parse("The EnvName should be set to the {{ .EnvName }} value. \n")
+	// here is the actual rendering takes place.
+	tmplContent.Execute(os.Stdout, devEnv)
 }
